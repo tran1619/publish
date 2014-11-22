6eeb587895cd14b106decfef5c1a6b63cb75a238
Michael <tran1619@gmail.com>
2014-11-22 09:25:47 -0500

    Add missing jar

--- publisher/src/publisher/web/SecurityFilter.java
+++ publisher/src/publisher/web/SecurityFilter.java
@@ -14,50 +14,50 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 
-public class SecurityFilter implements Filter 
-{
-   private Logger logger = Logger.getLogger(this.getClass());
-   public void init(FilterConfig filterConfig) throws ServletException
-   {
-   }
+public class SecurityFilter implements Filter {
+	private Logger logger = Logger.getLogger(this.getClass());
 
-   public void destroy() 
-   {
-   }
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
 
-   public void doFilter(
-         ServletRequest request, 
-         ServletResponse response,
-         FilterChain chain) 
-   throws IOException, ServletException 
-   {
-      logger.debug("doFilter()");
-      HttpServletResponse resp = (HttpServletResponse) response;
-      HttpServletRequest req = (HttpServletRequest) request;
-      String servletPath = req.getServletPath();
+	public void destroy() {
+	}
 
-      // Allow access to login functionality.
-      if (servletPath.equals("/login"))
-      {
-         chain.doFilter(req, resp);
-         return;
-      }
-      // Allow access to news feed.
-      if (servletPath.equals("/news.rss")) {
-	chain.doFilter(req, resp);
-	return;
-      }
-      // All other functionality requires authentication.
-      HttpSession session = req.getSession();
-      Long userId = (Long) session.getAttribute("userId");
-      if (userId != null)
-      {
-         // User is logged in.
-         chain.doFilter(req, resp);
-         return;
-      }
-      
-      // Request is not authorized.
-      resp.sendRedirect("login");
-   }   
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		logger.debug("doFilter()");
+		HttpServletResponse resp = (HttpServletResponse) response;
+		HttpServletRequest req = (HttpServletRequest) request;
+		String servletPath = req.getServletPath();
+
+		// Allow access to login functionality.
+		if (servletPath.equals("/login")) {
+			chain.doFilter(req, resp);
+			return;
+		}
+		// Allow access to news feed.
+		if (servletPath.equals("/news.rss")) {
+			chain.doFilter(req, resp);
+			return;
+		}
+		
+		// Allow access to web service
+		if(servletPath.equalsIgnoreCase("/publish"))
+		{
+			chain.doFilter(req, resp);
+			return;
+		}
+		
+		// All other functionality requires authentication.
+		HttpSession session = req.getSession();
+		Long userId = (Long) session.getAttribute("userId");
+		if (userId != null) {
+			// User is logged in.
+			chain.doFilter(req, resp);
+			return;
+		}
+
+		// Request is not authorized.
+		resp.sendRedirect("login");
+	}
 }
---
+++ publisher/src/publisher/ws/PublishNewsItemService.java
@@ -0,0 +1,73 @@
+package publisher.ws;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
+
+import publisher.data.NewsItem;
+import publisher.data.NewsItemDAO;
+
+public class PublishNewsItemService extends HttpServlet {
+	private Logger logger = Logger.getLogger(this.getClass());
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+		throws ServletException, IOException {
+			logger.debug("doPost()");
+			
+			// Read the XML request document
+			BufferedReader br = req.getReader();
+			SAXBuilder builder = new SAXBuilder();
+			Document requestDocument = null;
+			try
+			{
+				requestDocument = builder.build(br);
+			} catch (JDOMException e)
+			{
+				throw new RuntimeException(e);
+			}
+			
+			// Extract title and link from request
+			Element item = requestDocument.getRootElement();
+			Element titleElement = item.getChild("title");
+			String title = titleElement.getText();
+			Element linkElement = item.getChild("link");
+			String link = linkElement.getText();
+			
+			// Create news item from submitted data
+			NewsItem newsItem = new NewsItem();
+			newsItem.setTitle(title);
+			newsItem.setUrl(link);
+			new NewsItemDAO().create(newsItem);
+			
+			// Create response document with id of newly
+			// created news item
+			Element idElement = new Element("id");
+			idElement.addContent(newsItem.getId().toString());
+			Document responseDocument = new Document(idElement);
+			StringWriter sw = new StringWriter();
+			XMLOutputter outputter = new XMLOutputter();
+			outputter.output(responseDocument, sw);
+			String responseDocumentString = sw.toString();
+			
+			// Return response document
+			byte[] responseBytes = responseDocumentString.getBytes("UTF-8");
+			resp.setContentLength(responseBytes.length);
+			resp.setContentType("text/html");
+			OutputStream os = resp.getOutputStream();
+			os.write(responseBytes);
+			os.flush();
+	}
+}
